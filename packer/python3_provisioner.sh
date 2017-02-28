e94d6b685fe913ac09263dc7d42f183ec9e8ee65
kdaily <kdaily@users.noreply.github.com>
2017-02-27 22:44:23 -0800

    switch to centos7 for better python3 support

--- packer/python3_provisioner.sh
+++ packer/python3_provisioner.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
 
-yum install python35 python35-devel python35-pip
-pip-3.5 install virtualenv virtualenvwrapper snakemake synapseclient
+yum install python34 python34-devel python34-pip
+pip3.4 install virtualenv virtualenvwrapper snakemake synapseclient
